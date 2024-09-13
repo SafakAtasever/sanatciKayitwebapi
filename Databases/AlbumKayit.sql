1fba698b410706efd4b44a40c62a17215cc0883b
SafakAtasever <94807046+SafakAtasever@users.noreply.github.com>
2024-09-13 09:30:52 +0300

    Add files via upload

---
+++ Databases/AlbumKayit.sql
@@ -0,0 +1,48 @@
+﻿-- Hande Yener Albümleri
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Apayrı', '2006-01-01', 3600, 100000, 1);
+
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Hande Maxi', '2006-07-01', 2200, 50000, 1);
+
+-- Hadise Albümleri
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Aşk Kaç Beden Giyer?', '2011-04-01', 3100, 75000, 2);
+
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Tavsiye', '2014-08-01', 3300, 60000, 2);
+
+-- Murat Boz Albümleri
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Maximum', '2007-02-01', 3500, 120000, 3);
+
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Aşklarım Büyük Benden', '2011-05-01', 3700, 90000, 3);
+
+-- Mustafa Sandal Albümleri
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Gölgede Aynı', '1996-06-01', 4000, 200000, 4);
+
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Karizma', '2009-06-01', 3200, 85000, 4);
+
+-- Sagopa Kajmer Albümleri
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Kötü İnsanları Tanıma Senesi', '2008-01-01', 4500, 80000, 5);
+
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Pesimist EP 5', '2010-05-01', 3800, 70000, 5);
+
+-- Selena Gomez Albümleri
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Revival', '2015-10-09', 4200, 150000, 6);
+
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Rare', '2020-01-10', 3900, 120000, 6);
+
+-- Ceza Albümleri
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Rapstar', '2004-01-01', 4300, 200000, 7);
+
+INSERT INTO Albums (Name, ReleaseDate, Duration, SalesCount, ArtistId)
+VALUES ('Evim Sensin', '2010-01-01', 3700, 180000, 7);
---
+++ Databases/ArtistKayit.sql
@@ -0,0 +1,27 @@
+﻿-- Hande Yener
+INSERT INTO Artists (FirstName, LastName, BirthDate, Email)
+VALUES ('Hande', 'Yener', '1973-01-12', 'hande@example.com');
+
+-- Hadise
+INSERT INTO Artists (FirstName, LastName, BirthDate, Email)
+VALUES ('Hadise', 'Açıkgöz', '1985-10-22', 'hadise@example.com');
+
+-- Murat Boz
+INSERT INTO Artists (FirstName, LastName, BirthDate, Email)
+VALUES ('Murat', 'Boz', '1980-03-07', 'murat@example.com');
+
+-- Mustafa Sandal
+INSERT INTO Artists (FirstName, LastName, BirthDate, Email)
+VALUES ('Mustafa', 'Sandal', '1970-01-11', 'mustafa@example.com');
+
+-- Sagopa Kajmer
+INSERT INTO Artists (FirstName, LastName, BirthDate, Email)
+VALUES ('Yunus', 'Özyavuz', '1978-08-17', 'sagopa@example.com');
+
+-- Selena Gomez
+INSERT INTO Artists (FirstName, LastName, BirthDate, Email)
+VALUES ('Selena', 'Gomez', '1992-07-22', 'selena@example.com');
+
+-- Ceza
+INSERT INTO Artists (FirstName, LastName, BirthDate, Email)
+VALUES ('Bilgin', 'Ögünlü', '1977-12-31', 'ceza@example.com');
